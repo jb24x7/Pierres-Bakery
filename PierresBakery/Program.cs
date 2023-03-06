f3440c30ca70137e727c00e152a6aec6c411a9f9
James Provance <jb24x7@gmail.com>
2023-03-05 21:47:34 -0800

    finished ui

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,9 +8,14 @@ namespace PierresBakery
   {
     static void Main()
     {
-      Console.WriteLine("How many loaves of bread would you like?");
+      Console.WriteLine("Bread is 5$ per loaf and on sale for buy 3 get 1 free. How many loaves of bread would you like?");
       int loaves = int.Parse(Console.ReadLine());
-      Console.WriteLine(loaves);
+      Bread bread = new Bread(loaves);
+      Console.WriteLine("Pastries are 2$ per pastry and on sale for buy 4 get 1 free. How many pastries would you like?");
+      int pastriesNum = int.Parse(Console.ReadLine());
+      Pastries pastries = new Pastries(pastriesNum);
+      int costForAll = bread.BreadPrice() + pastries.PastriesPrice();
+      Console.WriteLine($"Your total will be ${costForAll} ");
     }
   }
 }
