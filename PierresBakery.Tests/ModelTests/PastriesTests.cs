1705a8e439268d65ed80a2215c9d08c4e6cfb4dd
James Provance <jb24x7@gmail.com>
2023-03-05 21:32:54 -0800

    added methods for pastries and tests

--- PierresBakery.Tests/ModelTests/PastriesTests.cs
+++ PierresBakery.Tests/ModelTests/PastriesTests.cs
@@ -1,7 +1,23 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using PierresBakery.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
 
-// namespace PierresBakery.Tests
-// {
+namespace PierresBakery.Tests
+{
+ [TestClass]
+  public class Pastriestests
+  {
+    [TestMethod]
+    public void PastriesConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Pastries pastries = new Pastries(6);
+      Assert.AreEqual(typeof(Pastries), pastries.GetType());
+    }
 
-// }
+       [TestMethod]
+    public void GetCost_ReturnsCostOfAllLoaves_Int()
+    {
+      Pastries pastries = new Pastries(6);
+      Assert.AreEqual(pastries.PastriesPrice(), 10);
+    }
+  }
+}
--- PierresBakery/Models/Pastries.cs
+++ PierresBakery/Models/Pastries.cs
@@ -2,6 +2,16 @@ namespace PierresBakery.Models
 {
   public class Pastries
   {
-
+    public int Order { get; set; }
+    public Pastries(int pastriesNumber)
+    {
+      Order = pastriesNumber;
+    }
+    public int PastriesPrice()
+    {
+      int cost = 2 * Order;
+      int finalCost = cost - (2 * ((Order - (Order % 4)) / 4));
+      return (finalCost);
+    }
   }
 }
